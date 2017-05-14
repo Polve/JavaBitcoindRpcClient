9138081e8cc7afd9856abf31d32612d71f2e3140
anton <admin@btcontract.com>
2017-05-14 22:33:19 +0300

    Add TxOut parsing test

--- src/test/java/wf/bitcoin/javabitcoindrpcclient/BitcoinJSONRPCClientTest.java
+++ src/test/java/wf/bitcoin/javabitcoindrpcclient/BitcoinJSONRPCClientTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import wf.bitcoin.krotjson.JSON;
 
 import java.util.LinkedList;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.core.Is.is;
@@ -96,5 +97,25 @@ public class BitcoinJSONRPCClientTest {
         String hex = client.signRawTransaction("0100000001B8B2244FACA910C1FFFF24ECD2B559B4699338398BF77E4CB1FDEB19AD419EA0010000001976A9144CB4C3B90994FEF58FABB6D8368302E917C6EFB188ACFFFFFFFF012E2600000000000017A9140B2D7ED4E5076383BA8E98B9B3BCE426B7A2EA1E8700000000");
         assertEquals("0100000001b8b2244faca910c1ffff24ecd2b559b4699338398bf77e4cb1fdeb19ad419ea0010000006b483045022100b68b7fe9cfabb32949af6747b6769dffcf2aa4170e4df2f0e9d0a4571989e94e02204cf506c210cdb6b6b4413bf251a0b57ebcf1b1b2d303ba6183239b557ef0a310012102ab46e1d7b997d8094e97bc06a21a054c2ef485fac512e2dc91eb9831af55af4effffffff012e2600000000000017a9140b2d7ed4e5076383ba8e98b9b3bce426b7a2ea1e8700000000",
                     hex);
+    }
+
+    @Test
+    public void txOutTest() throws Exception {
+        client = new MyClientTest(false, "gettxout", null,
+                "{\n" +
+                        "  \"bestblock\": \"55d2df879d7efc960628ae8d15352c904edfa5d716e31871cce6de5f05174986\",\n" +
+                        "  \"confirmations\": 551,\n" +
+                        "  \"value\": 0.02000000,\n" +
+                        "  \"scriptPubKey\": {\n" +
+                        "    \"asm\": \"0 955d569f9812bfe6796cbab7263f4cf90cc18c05c09a6e2eff3a890c2c5966af\",\n" +
+                        "    \"hex\": \"0020955d569f9812bfe6796cbab7263f4cf90cc18c05c09a6e2eff3a890c2c5966af\",\n" +
+                        "    \"type\": \"witness_v0_scripthash\"\n" +
+                        "  },\n" +
+                        "  \"version\": 2,\n" +
+                        "  \"coinbase\": false\n" +
+                        "}");
+
+        assertEquals(client.getTxOut("77c412efb79cbef9c13b40d2b146b529d399beced3c906df65bfb5973c3c2882", 0)
+                .scriptPubKey().hex(), "0020955d569f9812bfe6796cbab7263f4cf90cc18c05c09a6e2eff3a890c2c5966af");
     }
 }
